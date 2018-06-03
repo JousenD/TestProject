460eabaad798a53774ec74205abcc3aba377e469
Jousen-PC\Jousen <Romero87_2112@hotmail.com>
2018-06-03 22:05:51 +0200

    Something changed

--- Assets/Message.cs
+++ Assets/Message.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("You got it");
+		print("Something changed");
 	}
 	
 	// Update is called once per frame
